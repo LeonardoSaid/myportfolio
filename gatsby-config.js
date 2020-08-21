294d3422a846a85df60603d487b3bc7cc9763d9b
Leonardo Said <leonardocosta013@gmail.com>
2020-08-21 12:12:00 -0300

    updated resume and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Leonardo Said's Resume on the Web`,
+        short_name: `Leonardo Said's Resume`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
